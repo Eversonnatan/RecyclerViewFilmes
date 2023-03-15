99c70ea64ca4fc5ba226d25520dc3f04c8e5785c
everson.natan <everson.natan@gmail.com>
2023-03-14 21:30:22 -0300

    //criando adaptador

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/Filmes.java
@@ -1,4 +1,51 @@
 package br.com.etecia.recyclerviewfilmes;
 
 public class Filmes {
+    private String titulo;
+    private String descrcao;
+    private String categoria;
+    private int imagem;
+
+    //criar o constrtutor com parametros alt+insert
+
+    public Filmes(String titulo, String descrcao, String categoria, int imagem) {
+        this.titulo = titulo;
+        this.descrcao = descrcao;
+        this.categoria = categoria;
+        this.imagem = imagem;
+    }
+
+    //criando os metodos de acesso get an setter
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescrcao() {
+        return descrcao;
+    }
+
+    public void setDescrcao(String descrcao) {
+        this.descrcao = descrcao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
 }
