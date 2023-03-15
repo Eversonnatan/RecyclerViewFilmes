34a691e2307b5b26e014ccf3a61858509637c99d
everson.natan <everson.natan@gmail.com>
2023-03-14 22:15:04 -0300

    //criando adaptador e passando parametros

--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<Filmes> listaFilmes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //criando a base de dados para carregamento da Lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+                new Filmes("Jesus de Nazaré", "Desde o celebrado momento do seu nascimento até a sua chegada em Jerusalém para ir de encontro com a crucificação, a história de Jesus de Nazaré foi um marco que perdura até hoje. Pouco antes de cumprir seu destino na Terra, Jesus passa por um longo retiro de 40 dias pelo deserto da Judeia, atravessando diversos tipos de obstáculos, provações e tentações.","Histórico/Religioso",R.drawable.jesus));
+
+
+        //Criando a classe adaptadora e passando os parametros
+          MyAdapter adapter = new MyAdapter(getApplicationContext(),listaFilmes);
     }
 }
--- app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
+++ app/src/main/java/br/com/etecia/recyclerviewfilmes/MyAdapter.java
@@ -1,13 +1,28 @@
 package br.com.etecia.recyclerviewfilmes;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MyAdapter  extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
+    private Context mContexto;
+    private List<Filmes> listFilmes;
+
+    //criando construtor da classe com paramêtros
+
+    public MyAdapter(Context mContexto, List<Filmes> listFilmes) {
+        this.mContexto = mContexto;
+        this.listFilmes = listFilmes;
+    }
 
     @NonNull
     @Override
@@ -27,9 +42,15 @@ public class MyAdapter  extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     //criar a classe ViewHolder
     public class ViewHolder extends  RecyclerView.ViewHolder{
+        TextView idTituloFilmes;
+        ImageView idImgFilmes;
+        CardView idCardFilmes;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTituloFilmes = itemView.findViewById(R.id.idTituloFilmes);
+            idCardFilmes = itemView.findViewById(R.id.idCardFilmes);
+            idImgFilmes = itemView.findViewById(R.id.idImgFilmes);
         }
     }
 
